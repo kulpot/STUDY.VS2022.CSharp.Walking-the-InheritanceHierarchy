136a591fabcb5c976e68b0dc7f4c6187bc8fa93b
Kulpot <kulpotwahu@gmail.com>
2023-07-11 06:51:34 +0800

    Add project files.

---
+++ Walking the InheritanceHierarchy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=QnvdQgzives&list=PLRwVmtr-pp05brRDYXh-OTAIi-9kYcw3r&index=27
+//
+
+class MeBase { }
+class MeMid : MeBase { }
+class MeDerived : MeMid { }
+class MeMoreDerived : MeDerived { }
+class MeMegaDerived : MeMoreDerived { }
+
+class Mainclass
+{
+    static void Main()
+    {
+        Type type = typeof(MeMegaDerived);
+        while(type != null)
+        {
+            Console.WriteLine(type.Name);
+            type = type.BaseType;
+        }
+    }
+}
